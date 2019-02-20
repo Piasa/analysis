ddc0510c6dd5a55a787f5faa3467656446081fe5
Piasa <1005719151@qq.com>
2019-02-20 09:34:52 +0000

    change

--- analysis/ED.py
+++ analysis/ED.py
@@ -38,6 +38,36 @@ def alevelscore(cv):
     return alevel
 
 def analysisED(cv):
+    """criterion = nn.BCEWithLogitsLoss()  # 将 sigmoid 和 loss 写在一层，有更快的速度、更好的稳定性
+
+    w = nn.Parameter(torch.randn(2, 1))
+    b = nn.Parameter(torch.zeros(1))
+
+    def logistic_reg(x):
+        return torch.mm(x, w) + b
+
+    optimizer = torch.optim.SGD([w, b], 1.)
+    y_pred = logistic_reg(x_data)
+    loss = criterion(y_pred, y_data)
+    print(loss.data)
+    start = time.time()
+    for e in range(1000):
+        # 前向传播
+        y_pred = logistic_reg(x_data)
+        loss = criterion(y_pred, y_data)
+        # 反向传播
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # 计算正确率
+        mask = y_pred.ge(0.5).float()
+        acc = (mask == y_data).sum().data[0] / y_data.shape[0]
+        if (e + 1) % 200 == 0:
+            print('epoch: {}, Loss: {:.5f}, Acc: {:.5f}'.format(e + 1, loss.data[0], acc))
+
+    during = time.time() - start
+    print()
+    print('During Time: {:.3f} s'.format(during))"""
     if alevelscore(cv) + skillscore(cv) + employmentscore(cv) + hobbyscore(cv) >=60:
         return True
     else:
